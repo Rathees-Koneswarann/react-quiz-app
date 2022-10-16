1b72b592e42f5ff85e07f4cbf55d791ef8533007
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-16 13:33:44 +0530

    Add selection field

--- quiz-app/src/components/Settings.js
+++ quiz-app/src/components/Settings.js
@@ -3,26 +3,41 @@ import React, { useEffect, useState } from 'react'
 function Settings() {
     // useState hook 
     const [options, setOptions] = useState(null);
-
+    const [questionCategory, setQuestionCategory] = useState("");
     // useEffect hook
     useEffect(() => {
         const apiUrl = `https://opentdb.com/api_category.php`;
-        const controller = new AbortController();
-        const signal = controller.signal;
 
         fetch(apiUrl)
             .then((res) => res.json())
             .then((response) => {
                 setOptions(response.trivia_categories);
-            }).catch(err=>{
+            }).catch(err => {
                 console.log(err)
             })
 
     }, [setOptions]);
 
+    // event that is called when an option is chosen
+    const handleCategoryChange = event => {
+        setQuestionCategory(event.target.value)
+        console.log(questionCategory)
+    }
+
     return (
         <div>
-            <h2>Settings</h2>
+            <div>
+                <h2>Select Category:</h2>
+                <select value={questionCategory} onChange={handleCategoryChange}>
+                    <option>All</option>
+                    {options &&
+                        options.map((option) => (
+                            <option value={option.id} key={option.id}>
+                                {option.name}
+                            </option>
+                        ))}
+                </select>
+            </div>
         </div>
     )
 }
