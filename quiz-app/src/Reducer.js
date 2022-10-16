7bbe5db6c9b4add29e6c8abcc265ac21ad1717f7
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-16 14:17:02 +0530

    Update dispatching functions

--- quiz-app/src/Reducer.js
+++ quiz-app/src/Reducer.js
@@ -1,59 +1,65 @@
 const initState = {
     options: {
-      loading: false,
-      question_category: ``,
-      question_difficulty: ``,
-      question_type: ``,
-      amount_of_questions: 50
+        loading: false,
+        question_category: ``,
+        question_difficulty: ``,
+        question_type: ``,
+        amount_of_questions: 50
     }
-  }
+}
 
-  const Reducer = (state = initState, action) => {
+const Reducer = (state = initState, action) => {
     switch (action.type) {
-      case "CHANGE_LOADING":
-        return {
-          ...state,
-          options: {
-            ...state.options,
-            loading: action.value
-          }
-        }
-      case "CHANGE_CATEGORY":
-        return {
-          ...state,
-          options: {
-            ...state.options,
-            question_category: action.value
-          }
-        }
-      case "CHANGE_DIFFICULTY":
-        return {
-          ...state,
-          options: {
-            ...state.options,
-            question_difficulty: action.value
-          }
-        }
-      case "CHANGE_TYPE":
-        return {
-          ...state,
-          options: {
-            ...state.options,
-            question_type: action.value
-          }
-        }
-      
-      case "CHANGE_AMOUNT":
-        return {
-          ...state,
-          options: {
-            ...state.options,
-            amount_of_questions: action.value
-          }
-        }
-      default:
-        return state
+        case "CHANGE_LOADING":
+            return {
+                ...state,
+                options: {
+                    ...state.options,
+                    loading: action.value
+                }
+            }
+        case "CHANGE_CATEGORY":
+            return {
+                ...state,
+                options: {
+                    ...state.options,
+                    question_category: action.value
+                }
+            }
+        case "CHANGE_DIFFICULTY":
+            return {
+                ...state,
+                options: {
+                    ...state.options,
+                    question_difficulty: action.value
+                }
+            }
+        case "CHANGE_TYPE":
+            return {
+                ...state,
+                options: {
+                    ...state.options,
+                    question_type: action.value
+                }
+            }
+
+        case "CHANGE_AMOUNT":
+            return {
+                ...state,
+                options: {
+                    ...state.options,
+                    amount_of_questions: action.value
+                }
+            }
+
+        case "SET_QUESTIONS":
+            return {
+                ...state,
+                questions: action.questions
+            }
+        default:
+            return state
     }
-  }
-  
-  export default Reducer
+}
+
+export default Reducer
--- quiz-app/src/components/FetchButton.js
+++ quiz-app/src/components/FetchButton.js
@@ -1,35 +1,52 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 function FetchButton(props) {
-	// access the settings that will be used to construct the API query
-  const questionCategory = useSelector(state => state.options.question_category)
-  const questionDifficulty = useSelector(state => state.options.question_difficulty)
-  const questionType = useSelector(state => state.options.question_type)
-  const questionAmount = useSelector(state => state.options.amount_of_questions)
-  const questionIndex = useSelector(state => state.index)
-	
-  const handleQuery = async () => {
-		// we always need to specify the number of questions that we
-		// want to be returned
-    let apiUrl = `https://opentdb.com/api.php?amount=${questionAmount}`;
-		// only add the rest of the parameters if they aren't 'all'
-    if (questionCategory.length) {
-      apiUrl = apiUrl.concat(`&category=${questionCategory}`)
+    // access the settings that will be used to construct the API query
+    const questionCategory = useSelector(state => state.options.question_category)
+    const questionDifficulty = useSelector(state => state.options.question_difficulty)
+    const questionType = useSelector(state => state.options.question_type)
+    const questionAmount = useSelector(state => state.options.amount_of_questions)
+    const questionIndex = useSelector(state => state.index)
+
+    const dispatch = useDispatch()
+    const setLoading = value => {
+        dispatch({
+            type: 'CHANGE_LOADING',
+            loading: value
+        })
     }
-    if (questionDifficulty.length) {
-      apiUrl = apiUrl.concat(`&difficulty=${questionDifficulty}`)
+    const setQuestions = value => {
+        dispatch({
+            type: 'SET_QUESTIONS',
+            questions: value
+        })
     }
-    if (questionType.length) {
-      apiUrl = apiUrl.concat(`&type=${questionType}`)
+
+    const handleQuery = async () => {
+        // we always need to specify the number of questions that we
+        // want to be returned
+        let apiUrl = `https://opentdb.com/api.php?amount=${questionAmount}`;
+        // only add the rest of the parameters if they aren't 'all'
+        if (questionCategory.length) {
+            apiUrl = apiUrl.concat(`&category=${questionCategory}`)
+        }
+        if (questionDifficulty.length) {
+            apiUrl = apiUrl.concat(`&difficulty=${questionDifficulty}`)
+        }
+        if (questionType.length) {
+            apiUrl = apiUrl.concat(`&type=${questionType}`)
+        }
+        setLoading(true);
+        await fetch(apiUrl)
+            .then((res) => res.json())
+            .then((response) => {
+                // set questions in the state using an action
+                setQuestions(response.results)
+                setLoading(false);
+            });
     }
-    await fetch(apiUrl)
-      .then((res) => res.json())
-      .then((response) => {
-        // this is where we will set questions in the state using an action
-      });
-  }
-	// we will resuse this component, so the button text will be passed as props
-  return <button onClick={handleQuery}>{props.text}</button>;
+    // we will resuse this component, so the button text will be passed as props
+    return <button onClick={handleQuery}>{props.text}</button>;
 }
 
 export default FetchButton;
