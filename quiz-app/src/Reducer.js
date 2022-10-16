557060ceb944bc82321dfc4589eda56c04a871a9
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-16 13:54:54 +0530

    Add Reducer.js

---
+++ quiz-app/src/Reducer.js
@@ -0,0 +1,10 @@
+const initState = {
+    options: {
+      loading: false,
+      question_category: ``,
+      question_difficulty: ``,
+      question_type: ``,
+      amount_of_questions: 50
+    }
+  }
+  export default Reducer
