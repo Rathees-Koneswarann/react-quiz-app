335967dfc7cd0b366f7a99c7f96a13cf32d0313d
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-16 14:41:01 +0530

    Cleanup finalscreen

--- quiz-app/src/components/FinalScreen.js
+++ quiz-app/src/components/FinalScreen.js
@@ -1,29 +1,37 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+
 import FetchButton from './FetchButton'
+
 function FinalScreen() {
   const score = useSelector((state) => state.score)
+
   const dispatch = useDispatch()
+
   const replay = () => {
     dispatch({
       type: 'SET_INDEX',
       index: 0,
     })
+
     dispatch({
       type: 'SET_SCORE',
       score: 0,
     })
   }
+
   const settings = () => {
     dispatch({
       type: 'SET_QUESTIONS',
       questions: [],
     })
+
     dispatch({
       type: 'SET_SCORE',
       score: 0,
     })
   }
+
   return (
     <div>
       <h3>Final Score: {score}</h3>
