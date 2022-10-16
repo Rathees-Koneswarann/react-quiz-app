2a86ac335476af60b9e041cfaf57a97c405ae31f
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-16 13:30:32 +0530

    Write function to fetch catergaries

--- quiz-app/src/components/Settings.js
+++ quiz-app/src/components/Settings.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Settings() {
-  return (
-    <div>
-      <h2>Settings</h2>
-    </div>
-  )
+    // useState hook 
+    const [options, setOptions] = useState(null);
+
+    // useEffect hook
+    useEffect(() => {
+        const apiUrl = `https://opentdb.com/api_category.php`;
+        const controller = new AbortController();
+        const signal = controller.signal;
+
+        fetch(apiUrl)
+            .then((res) => res.json())
+            .then((response) => {
+                setOptions(response.trivia_categories);
+            }).catch(err=>{
+                console.log(err)
+            })
+
+    }, [setOptions]);
+
+    return (
+        <div>
+            <h2>Settings</h2>
+        </div>
+    )
 }
 
 export default Settings
