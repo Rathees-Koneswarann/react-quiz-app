6e9f7175aa8729ed1032ecb951ff31c7f3e0c213
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-16 13:55:35 +0530

    Add reducer function

--- quiz-app/src/Reducer.js
+++ quiz-app/src/Reducer.js
@@ -7,4 +7,53 @@ const initState = {
       amount_of_questions: 50
     }
   }
+
+  const Reducer = (state = initState, action) => {
+    switch (action.type) {
+      case "CHANGE_LOADING":
+        return {
+          ...state,
+          options: {
+            ...state.options,
+            loading: action.value
+          }
+        }
+      case "CHANGE_CATEGORY":
+        return {
+          ...state,
+          options: {
+            ...state.options,
+            question_category: action.value
+          }
+        }
+      case "CHANGE_DIFFICULTY":
+        return {
+          ...state,
+          options: {
+            ...state.options,
+            question_difficulty: action.value
+          }
+        }
+      case "CHANGE_TYPE":
+        return {
+          ...state,
+          options: {
+            ...state.options,
+            question_type: action.value
+          }
+        }
+      
+      case "CHANGE_AMOUNT":
+        return {
+          ...state,
+          options: {
+            ...state.options,
+            amount_of_questions: action.value
+          }
+        }
+      default:
+        return state
+    }
+  }
+  
   export default Reducer
